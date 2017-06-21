5c970fcf1b90ad9890784eb3dc0426149ea01173
adeo8 <aormond@purdue.edu>
2017-06-21 14:53:32 -0400

    Updated Comments

--- inc/bezelplugin.h
+++ inc/bezelplugin.h
@@ -1,25 +1,24 @@
 #ifndef _BEZELPLUGIN_H_
 #define _BEZELPLUGIN_H_
 
-/**
- * This header file is included to define _EXPORT_.
- */
 #include <stdbool.h>
 #include <tizen.h>
 #include <efl_extension.h>
 #include <dlog.h>
 
 
+Eina_Bool _rotary_handler_cb(void *data, Eext_Rotary_Event_Info *ev);
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-// This method is exported from bezelplugin.so
+
+
+// These methods are exported from bezelplugin.so
 EXPORT_API bool tizenSetup(void);
 EXPORT_API int getDir(void);
 EXPORT_API void resetDir(void);
-Eina_Bool _rotary_handler_cb(void *data, Eext_Rotary_Event_Info *ev);
 
 
 #ifdef __cplusplus
--- src/bezelplugin.c
+++ src/bezelplugin.c
@@ -1,37 +1,45 @@
-/**
- * This file contains the exported symbol.
- */
 #include "bezelplugin.h"
 #define PR_TAG "bezelplugin"
-// This is an example of an exported method.
 
 int dir = 0;
 
+/*
+ * Initializes Plugin
+ */
 EXPORT_API bool tizenSetup(void)
 {
-	dlog_print(DLOG_DEBUG, PR_TAG, "Setup started");
+//	dlog_print(DLOG_DEBUG, PR_TAG, "Setup started");
 	eext_rotary_event_handler_add(_rotary_handler_cb, NULL);
 	return true;
 }
 
+/*
+ * Returns internal tracker
+ */
 EXPORT_API int getDir(void){
 	return dir;
 }
 
+/*
+ * Resets internal tracker
+*/
 EXPORT_API void resetDir(void){
 	dir = 0;
 }
 
+/*
+ * Function for handling native rotation events
+ */
 Eina_Bool _rotary_handler_cb(void *data, Eext_Rotary_Event_Info *ev)
 {
    if (ev->direction == EEXT_ROTARY_DIRECTION_CLOCKWISE)
    {
-      dlog_print(DLOG_DEBUG, PR_TAG, "ROTARY HANDLER: Rotary device rotated in clockwise direction");
+//      dlog_print(DLOG_DEBUG, PR_TAG, "ROTARY HANDLER: Rotary device rotated in clockwise direction");
       dir = -1;
    }
    else
    {
-      dlog_print(DLOG_DEBUG, PR_TAG, "Rotary device rotated in counter clockwise direction");
+//      dlog_print(DLOG_DEBUG, PR_TAG, "Rotary device rotated in counter clockwise direction");
       dir = 1;
    }
 
